4b16e0deac106da002b6f4c12cdbcffdc885fb07
yxg1217 <694948161@qq.com>
2019-03-27 11:27:21 +0100

    Update server.js
    change time stamps and interval

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 
 var http = require('http');
-var t1 = new Date().getTime();
-console.log("timestamp: ",t1);
-var t2 = t1;
-var interval = 0;
 var server = http.createServer(function(req,res){
 	res.write("hello world");
+	var t1 = new Date().getTime();
+	var t2 = t1;
+	var interval = 0;
+	console.log("timestamp: ",t1);
 	console.log("hello docker");
 	t2 = Date.now()
 	interval = (t2 - t1);
+	console.log("interval: ", interval);
 	res.end();
 });
 server.listen(8000);
 
-t2 = Date.now();
-interval = (t2 - t1);
-console.log("interval: ", interval);
